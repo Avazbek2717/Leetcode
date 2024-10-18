68e0dc6962a6f1d147fa8057580f553de7c8ed8d
Avazbek <ax1of710@gmail.com>
2024-10-18 14:05:25 +0500

    tree node

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,36 @@
-a= 20
-b= 30
-print(a+b)
-print(a-b)
-print(a*b)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curSum):
+            if not node:
+                return False
+            curSum = curSum + node.val
+            if not node.left and not node.right:
+                return curSum == targetSum
+            
+            return dfs(node.left, curSum) or dfs(node.right, curSum)
+        
+        return dfs(root, 0)
+
+
+            
+
+
+# root = TreeNode(5)
+# root.left = TreeNode(4)
+# root.right = TreeNode(8)
+# root.left.left = TreeNode(11)
+# root.left.left.left = TreeNode(7)
+# root.left.left.right = TreeNode(2)
+# root.right.left = TreeNode(13)
+# root.right.right = TreeNode(4)
+# root.right.right.right = TreeNode(1)
+# targetSum  = 22
+
+
+        
